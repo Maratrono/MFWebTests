066f129422eb4d39e103fc3f34e572a2dd89fa55
Maratrono <snab008@mail.ru>
2026-06-21 10:58:43 +0300

    Повторил Романа, добавили фраемворк allure

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 #Позволяет программе понять, чего именно мы ждем. Например: «жди, пока кнопка станет кликабельной», «жди, пока исчезнет баннер» или «жди, пока появится нужный текст»
 from selenium.webdriver.support import expected_conditions
+import allure
 
 # Создаем родительский» класс-шаблон. От него будут наследоваться все остальные страницы вашего тестов
 # Магическая функция init self чтобы driver был доступен во всех функциях создаваемых под этим классом
@@ -18,6 +19,10 @@ class BasePage:
         return WebDriverWait(self.driver, time).until(expected_conditions.visibility_of_element_located(locator), message= f"Не удалось найти элемент {locator}")
 
     #Создаем функцию для перехода к сайту
+    @allure.step("Открываем страницу")
     def get_url(self, url):
         return self.driver.get(url)
 
+
+    def attach_screenshot(self):
+       allure.attach(self.driver.get_screenshot_as_png(), "скриншот", allure.attachment_type.PNG)
--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,3 +1,5 @@
+import allure
+
 from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
@@ -6,15 +8,24 @@ BASE_URL = "https://ok.ru/"
 EMPTY_LOGIN_ERROR = "Введите логин"
 EMPTY_PASSWORD_ERROR = "Введите пароль"
 
+
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки авторизации при пустом поле логина")
 def test_empty_login(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
+    import time;time.sleep(1.5)
+    # Добавил эту строчку, чтобы Одноклассники успели показать ошибку до скриншота, а то скриншот у меня был без текста ошибки
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки авторизации при пустом поле пароля")
 def test_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.send_keys_login(text_for_send= "admin")
     LoginPage.click_login()
+    import time;time.sleep(1.5)
+    # Добавил эту строчку, чтобы Одноклассники успели показать ошибку до скриншота, а то скриншот у меня был без текста ошибки
     assert LoginPage.get_error_text() == EMPTY_PASSWORD_ERROR
